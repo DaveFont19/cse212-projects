003b935a162d399face4a185822f757e86b6c964
DaveFont19 <dfontes@byupathway.edu>
2025-11-06 21:35:49 -0600

    week01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -35,7 +35,7 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-        //I will use the amount number to find the index where the list have to start and make a loop
+        //I will use the amount number to find the index where the list have to start, clear the list and insert the new data
         int count = data.Count;
         int headCount = count - amount;
 
