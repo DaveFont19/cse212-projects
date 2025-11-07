0a1015cc945dd807b0ad096798245a0b985fa489
DaveFont19 <dfontes@byupathway.edu>
2025-11-06 21:35:12 -0600

    Week01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // First I decided to use a for loop where I will use the length as the number of multiples
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            double multiple = number * (i + 1);
+            multiples[i] = multiple;
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +35,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //I will use the amount number to find the index where the list have to start and make a loop
+        int count = data.Count;
+        int headCount = count - amount;
+
+        List<int> tail = data.GetRange(headCount, amount);
+        List<int> head = data.GetRange(0, headCount);
+
+        data.Clear();
+        data.AddRange(tail);
+        data.AddRange(head);
     }
 }
